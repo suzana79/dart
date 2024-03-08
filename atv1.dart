8b1c6a9a2e7f2d7853e2a28fce5ecacc7913cfd2
suzana79 <128698630+suzana79@users.noreply.github.com>
2024-03-08 11:32:53 -0300

    Create atv1.dart

---
+++ atv1.dart
@@ -0,0 +1,11 @@
+void main() {
+  String nome = 'Suzana';
+  String generoFavorito = 'corrida';
+  String jogoFavorito = 'Real Racing 3';
+  String tipoPlataforma = 'console';
+  
+  print('Olá, meu nome é ${nome}, meu gênero favorito de jogo é ${generoFavorito} e o meu jogo favorito é ${jogoFavorito} e jogo no ${tipoPlataforma}');
+}  
+  
+  
+  
