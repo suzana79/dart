4d7da23d920362a6d1c1704636205bef07d46e41
suzana79 <128698630+suzana79@users.noreply.github.com>
2024-03-08 11:57:11 -0300

    Create atv3.dart

---
+++ atv3.dart
@@ -0,0 +1,13 @@
+void main() {
+  String pessoa = 'Lara';
+  double peso = 75;
+  double altura = 1.81;
+  double imc = (peso / (altura * altura));
+  print('A ${pessoa} tem o imc de ${imc.toStringAsFixed(2)}');
+  
+}  
+  
+  
+  
+  
+  
